b0e627a12b5f17cef69d62b9b9eca2e672fe675f
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-08-27 02:09:12 +0530

    some delete fun

--- analog-clock/clock.js
+++ analog-clock/clock.js
@@ -17,20 +17,4 @@ function rotateClockHand(element,rotation)
 {
     element.style.setProperty('--rotate',rotation * 360);
 }
-// setInterval(clockTick , 1000)
-const setTimeButton = document.getElementById('set-time-button');
-        setTimeButton.addEventListener('click', () => {
-            const hours = parseInt(document.getElementById('hour-input').value) || 0;
-            const minutes = parseInt(document.getElementById('minute-input').value) || 0;
-            const seconds = parseInt(document.getElementById('second-input').value) || 0;
-
-            const totalSeconds = hours * 3600 + minutes * 60 + seconds;
-
-            const newRotation = totalSeconds / 43200; // 12 hours in seconds
-
-            rotateClockHand(secondhand, newRotation);
-            rotateClockHand(minutehand, newRotation * 12);
-            rotateClockHand(hourhand, newRotation * 12);
-        });
-
-        setInterval(clockTick, 1000);
+setInterval(clockTick , 1000)
